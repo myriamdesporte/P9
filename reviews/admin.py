d8edf3d586d5ed2e855f1a8456fe0e5490b025f3
Myriam Desporte <myriam.desporte@gmail.com>
2025-12-03 16:12:05 +0100

    Add Admin classes to handle display

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Ticket, Review, UserFollows
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'time_created')
+    list_filter = ('user', 'time_created')
+    search_fields = ('title', 'description')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'rating', 'user', 'ticket', 'time_created')
+    list_filter = ('rating', 'user')
+    search_fields = ('headline', 'body')
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user')
+    search_fields = ('user__username', 'followed_user__username')
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
