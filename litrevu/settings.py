615f95ce5fb937bdc80c764e7dd710c415a0ba38
Myriam Desporte <myriam.desporte@gmail.com>
2025-12-12 14:27:57 +0100

    Add MEDIA_ROOT and MEDIA_URL settings for file uploads

--- litrevu/settings.py
+++ litrevu/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+UPLOAD_PATH = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = UPLOAD_PATH
